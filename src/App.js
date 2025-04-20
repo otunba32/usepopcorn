8ce288fd2270d138cfe91fe9fbc560f76c8dbe43
Emmanuel Abia <emmanuel.abia32@gmail.com>
2025-04-20 09:02:08 -0400

    cleaning up the title

--- src/App.js
+++ src/App.js
@@ -308,6 +308,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   useEffect(function() {
     if(!title) return;
     document.title = `Movie | ${title}`;
+
+    return function() {
+      document.title = "usePopcorn"
+    }
   }, [title])
 
 
